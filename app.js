f5d4d685268516a873509e0ad7df674536551744
shubhamgupta608 <shubham.gupta@venturepact.com>
2018-04-10 17:19:39 +0530

    new feature completed

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 function aBIgNewFeature(){
     console.log("writing a lot of code");
     console.log("Halfway donw with this new feature now.");
+    console.log("Completely done with this new feature now.");
+    
 }
 function something(){
 console.log("something");
