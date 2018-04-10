0bb9617c69be478ce3da13e30db0083d6b9895dc
shubhamgupta608 <shubham.gupta@venturepact.com>
2018-04-10 16:59:50 +0530

    halfway done with new feature.

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
+function aBIgNewFeature(){
+    console.log("writing a lot of code");
+    console.log("Halfway donw with this new feature now.");
+}
 function something(){
 console.log("something");
 }
 console.log("hello world");
 something();
+aBIgNewFeature();
